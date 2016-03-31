5e0e16c4ee68b0337befb9f318120f3092918e37
Alex Gusev <alex@flancer64.com>
2016-03-31 12:50:21 +0300

    MOBI-265 - Merge Common & M2 modules and clean up extra code (Setup/Schema&Data)

--- src/Setup/InstallData.php
+++ src/Setup/InstallData.php
@@ -7,11 +7,11 @@
 namespace Praxigento\Wallet\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
-use Praxigento\Core\Setup\Data\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
 
 class InstallData extends \Praxigento\Core\Setup\Data\Base
 {
-    protected function _setup(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         // TODO: Implement _setup() method.
     }
