3dd9d330f6554e5b4fb3e98107228ad0a8cb588a
Alex Gusev <alex@flancer64.com>
2016-03-31 15:08:32 +0300

    MOBI-265 - data setup migration

--- src/Setup/InstallData.php
+++ src/Setup/InstallData.php
@@ -8,13 +8,47 @@ namespace Praxigento\Wallet\Setup;
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Praxigento\Accounting\Lib\Entity\Type\Asset as TypeAsset;
+use Praxigento\Accounting\Lib\Entity\Type\Operation as TypeOperation;
+use Praxigento\Wallet\Lib\Config as Cfg;
 
 class InstallData extends \Praxigento\Core\Setup\Data\Base
 {
-    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    private function _addAccountingAssetsTypes()
     {
-        // TODO: Implement _setup() method.
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeAsset::ENTITY_NAME),
+            [TypeAsset::ATTR_CODE, TypeAsset::ATTR_NOTE],
+            [
+                [
+                    Cfg::CODE_TYPE_ASSET_WALLET_ACTIVE,
+                    'Active funds in a customer wallet. Can be transferred to other customer, to external payment system or used to sale smth.'
+                ],
+                [
+                    Cfg::CODE_TYPE_ASSET_WALLET_HOLD,
+                    '\'On hold\' funds in a customer wallet. Can be converted to active funds only.'
+                ]
+            ]
+        );
     }
 
+    private function _addAccountingOperationsTypes()
+    {
+        $this->_getConn()->insertArray(
+            $this->_getTableName(TypeOperation::ENTITY_NAME),
+            [TypeOperation::ATTR_CODE, TypeOperation::ATTR_NOTE],
+            [
+                [
+                    Cfg::CODE_TYPE_OPER_WALLET_TRANSFER,
+                    'Transfer WALLET_ACTIVE asset between customers accounts (or customer & representative accounts).'
+                ]
+            ]
+        );
+    }
 
+    protected function _setup(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->_addAccountingAssetsTypes();
+        $this->_addAccountingOperationsTypes();
+    }
 }
