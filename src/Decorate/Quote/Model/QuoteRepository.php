4f95c4d564b1857b2082376390d721a47001d890
Alex Gusev <alex@flancer64.com>
2016-10-31 13:28:04 +0200

    MOBI-488 - Clean up partial payment registry before quote clena up

---
+++ src/Decorate/Quote/Model/QuoteRepository.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Wallet\Decorate\Quote\Model;
+
+/**
+ * @SuppressWarnings(PHPMD.CamelCasePropertyName)
+ */
+class QuoteRepository
+{
+    /** @var \Praxigento\Wallet\Repo\Entity\Partial\IQuote */
+    protected $_repoPartialQuote;
+
+    public function __construct(
+        \Praxigento\Wallet\Repo\Entity\Partial\IQuote $repoPartialQuote
+    ) {
+        $this->_repoPartialQuote = $repoPartialQuote;
+    }
+
+    /**
+     * Delete partial payment registry data before quote deletion.
+     *
+     * @param $subject
+     * @param \Magento\Quote\Api\Data\CartInterface $quote
+     * @return array
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function beforeDelete(
+        $subject,
+        \Magento\Quote\Api\Data\CartInterface $quote
+    ) {
+        $quoteId = $quote->getId();
+        $this->_repoPartialQuote->deleteById($quoteId);
+        return [$quote];
+    }
+}
