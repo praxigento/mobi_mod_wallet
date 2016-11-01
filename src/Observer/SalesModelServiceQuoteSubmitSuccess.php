e63ceec7e93118978224bac81537b7b7c351dbd5
Alex Gusev <alex@flancer64.com>
2016-11-01 15:12:09 +0200

    MOBI-493 - Populate partial registers

--- src/Observer/SalesModelServiceQuoteSubmitSuccess.php
+++ src/Observer/SalesModelServiceQuoteSubmitSuccess.php
@@ -15,26 +15,39 @@ class SalesModelServiceQuoteSubmitSuccess
     const DATA_QUOTE = 'quote';
     /** @var \Psr\Log\LoggerInterface */
     protected $_logger;
-    /** @var \Praxigento\Wallet\Repo\Entity\Partial\IQuote */
-    protected $_repoPartialQuote;
     /** @var \Praxigento\Wallet\Repo\Entity\Partial\ISale */
     protected $_repoPartialSale;
 
     public function __construct(
         \Psr\Log\LoggerInterface $logger,
-        \Praxigento\Wallet\Repo\Entity\Partial\IQuote $repoPartialQuote,
         \Praxigento\Wallet\Repo\Entity\Partial\ISale $repoPartialSale
     ) {
         $this->_logger = $logger;
-        $this->_repoPartialQuote = $repoPartialQuote;
         $this->_repoPartialSale = $repoPartialSale;
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $order = $observer->getData(self::DATA_ORDER);
+        /* Get base amount for partial payment from quote totals */
+        /** @var \Magento\Quote\Model\Quote $quote */
         $quote = $observer->getData(self::DATA_QUOTE);
-
+        $basePartialAmount = $quote->getShippingAddress()
+            ->getData(\Praxigento\Wallet\Model\Quote\Address\Total\Partial::CODE_BASE_TOTAL);
+        if ($basePartialAmount) {
+            /* save amounts into order registry */
+            $partialAmount = $quote->getShippingAddress()
+                ->getData(\Praxigento\Wallet\Model\Quote\Address\Total\Partial::CODE_TOTAL);
+            /** @var \Magento\Sales\Model\Order $order */
+            $order = $observer->getData(self::DATA_ORDER);
+            $orderId = $order->getId();
+            $data = new \Praxigento\Wallet\Data\Entity\Partial\Sale();
+            $data->setPartialAmount($partialAmount);
+            $data->setBasePartialAmount($basePartialAmount);
+            $data->setSaleOrderRef($orderId);
+            $this->_repoPartialSale->create($data);
+            $this->_logger->debug("New partial payment by eWallet is registered for order #$orderId "
+                . "(base: '$basePartialAmount', amount: '$partialAmount').");
+        }
     }
 
 }
