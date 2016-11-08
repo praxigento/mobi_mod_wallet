1813d941c3605b78b459d3c20442cc4430e15748
Alex Gusev <alex@flancer64.com>
2016-11-08 17:32:44 +0200

    MOBI-497 - Correct invoice amount on partial payment

--- src/Block/Adminhtml/Order/Invoice/Total/Partial.php
+++ src/Block/Adminhtml/Order/Invoice/Total/Partial.php
@@ -11,6 +11,19 @@ class Partial
 {
     /** see src/view/adminhtml/layout/sales_order_invoice_new.xml */
     const CODE = Cfg::CODE_TOTAL_PARTIAL;
+    /** @var \Praxigento\Wallet\Repo\Entity\Partial\ISale */
+    protected $repoPartialSale;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Sales\Helper\Admin $adminHelper,
+        \Praxigento\Wallet\Repo\Entity\Partial\ISale $repoPartialSale,
+        array $data = []
+    ) {
+        parent::__construct($context, $registry, $adminHelper, $data);
+        $this->repoPartialSale = $repoPartialSale;
+    }
 
     /**
      * Initialize partial payment related totals (if exist).
@@ -23,13 +36,23 @@ class Partial
         $parent = $this->getParentBlock();
         /** @var \Magento\Sales\Model\Order $order */
         $order = $parent->getOrder();
+        /* check collected partial totals (for currently processed invoices only) */
         $invoices = $order->getInvoiceCollection();
         $invoice = $invoices->getFirstItem();
         $partialBase = $invoice->getData(Cfg::CODE_TOTAL_PARTIAL_AMOUNT_BASE);
         if ($partialBase) {
-            /* add partial total */
-            $partialBase = $invoice->getData(Cfg::CODE_TOTAL_PARTIAL_AMOUNT_BASE);
             $partial = $invoice->getData(Cfg::CODE_TOTAL_PARTIAL_AMOUNT);
+        } else {
+            /* check sales registry in repo */
+            $orderId = $order->getId();
+            $found = $this->repoPartialSale->getById($orderId);
+            if ($found) {
+                $partialBase = $found->getBasePartialAmount();
+                $partial = $found->getBasePartialAmount();
+            }
+        }
+        if ($partialBase) {
+            /* add partial total */
             $total = new \Magento\Framework\DataObject(
                 [
                     'code' => self::CODE,
